822e1c1fcfaa577b88f9bd6fb1d4aed3b7aca9c2
Zkine <philippe_jaymes@hotmail.com>
2023-11-30 13:20:24 +0100

    les notes de frais s'affichent par ordre décroissant

--- Billed-app-FR-Front/src/__tests__/Bills.test.js
+++ Billed-app-FR-Front/src/__tests__/Bills.test.js
@@ -42,7 +42,7 @@ describe("Given I am connected as an employee", () => {
           /^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i
         )
         .map((a) => a.innerHTML);
-      const antiChrono = (a, b) => (a > b ? 1 : -1);
+      const antiChrono = (a, b) => (Date.parse(a) < Date.parse(b) ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       console.log(datesSorted);
       // Set ignore order
--- Billed-app-FR-Front/src/containers/Bills.js
+++ Billed-app-FR-Front/src/containers/Bills.js
@@ -47,30 +47,28 @@ export default class {
         .bills()
         .list()
         .then((snapshot) => {
-          const bills = snapshot.map((doc) => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status),
-              };
-            } catch (e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e, "for", doc);
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status),
-              };
-            }
-          });
+          const bills = snapshot
+            .map((doc) => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  status: formatStatus(doc.status),
+                };
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, "for", doc);
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status),
+                };
+              }
+            })
+            .sort((a, b) => (Date.parse(a.date) < Date.parse(b.date) ? 1 : -1));
 
-          const antiChrono = (a, b) => (a > b ? 1 : -1);
-          const datesSorted = [...bills].sort(antiChrono);
-          console.log(datesSorted);
-          console.log("length", bills.length);
-          return datesSorted;
+          return bills;
         });
     }
   };
